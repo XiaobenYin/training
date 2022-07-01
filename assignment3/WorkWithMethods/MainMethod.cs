e6ed5bbb4697987393e1115ab2fbba9d52df5ab5
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-06-30 22:20:10 -0700

    Update MainMethod.cs

--- assignment3/WorkWithMethods/MainMethod.cs
+++ assignment3/WorkWithMethods/MainMethod.cs
@@ -4,35 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace assignment3.WorkWithMethods
-//{
-//    internal class MainMethod
-//    {
-//        static void Main(string[] args)
-//        {
-//            /* for question #1 */
-//            Console.WriteLine("Question#1 - Dealing with Array:");
-//            int[] numbers = WorkWithMethods1.GenerateNumbers(5);
-//            Console.WriteLine("The original array is: ");
-//            WorkWithMethods1.PrintNumbers(numbers);
-//            int[] reversedArr = WorkWithMethods1.Reverse(numbers);
-//            Console.WriteLine("\nThe reversed array is: ");
-//            WorkWithMethods1.PrintNumbers(reversedArr);
+namespace assignment3.WorkWithMethods
+{
+   internal class MainMethod
+   {
+       static void Main(string[] args)
+       {
+           /* for question #1 */
+           Console.WriteLine("Question#1 - Dealing with Array:");
+           int[] numbers = WorkWithMethods1.GenerateNumbers(5);
+           Console.WriteLine("The original array is: ");
+           WorkWithMethods1.PrintNumbers(numbers);
+           int[] reversedArr = WorkWithMethods1.Reverse(numbers);
+           Console.WriteLine("\nThe reversed array is: ");
+           WorkWithMethods1.PrintNumbers(reversedArr);
 
-//            /* for question #2 */
-//            Console.WriteLine("\nQuestion#2 - Fibonacci Numbers");
-//            int num = FibonacciProblem.Fibonacci(10);
-//            Console.WriteLine(num);
-//            Console.WriteLine();
+           /* for question #2 */
+           Console.WriteLine("\nQuestion#2 - Fibonacci Numbers");
+           int num = FibonacciProblem.Fibonacci(10);
+           Console.WriteLine(num);
+           Console.WriteLine();
 
-//            Console.WriteLine("First ten Fibonacci numbers: ");
-//            int[] firstTenFibNum = new int[10];
+           Console.WriteLine("First ten Fibonacci numbers: ");
+           int[] firstTenFibNum = new int[10];
 
-//            for (int i = 0; i < firstTenFibNum.Length; i++)
-//            {
-//                firstTenFibNum[i] = FibonacciProblem.Fibonacci(i + 1);
-//                Console.Write($"{firstTenFibNum[i]} ");
-//            }
-//        }
-//    }
-//}
+           for (int i = 0; i < firstTenFibNum.Length; i++)
+           {
+               firstTenFibNum[i] = FibonacciProblem.Fibonacci(i + 1);
+               Console.Write($"{firstTenFibNum[i]} ");
+           }
+       }
+   }
+}
