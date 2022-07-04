94a188bab448f15fd9d5ace7fbfdf203b8f949eb
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-07-04 00:05:47 -0700

    finished C# assignment 4

---
+++ assignment4/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public class GenericRepository<T> where T : Entity
+    {
+        List<T> list = new List<T>();
+
+        public void Add(T entity)
+        {
+            list.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return list;
+        }
+
+        public T GetById(int id)
+        {
+            foreach (T i in list)
+            {
+                if (i.Id == id)
+                {
+                    return i;
+                }
+            }
+            return null;
+
+        }
+
+        public void Remove(T item)
+        {
+            list.Remove(item);
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ assignment4/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    interface IRepository<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Save();
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+    }
+}
